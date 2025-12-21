fc53e44dc2e47493ca42f61fd2da65c065ee67aa
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2025-12-21 23:51:32 +0530

    Delete FinalTableSchema.sql

--- FinalTableSchema.sql
+++
@@ -1,7 +0,0 @@
-select * from Users
-select * from UserAvailabilities
-select * from UserNotificationPreferences
-select * from UserPrivacy
-select * from UserProfiles
-select * from UserReputation
-select * from UserSkills
