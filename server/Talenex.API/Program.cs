30de284ea0e140a18f4a592618a23d3a8fb4cfb1
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2026-02-02 16:37:52 +0530

    docker file is added

--- server/Talenex.API/Program.cs
+++ server/Talenex.API/Program.cs
@@ -24,6 +24,8 @@ var builder = WebApplication.CreateBuilder(args);
 // ==========================
 builder.Services.AddControllers();
 
+builder.WebHost.UseUrls("http://0.0.0.0:10000");
+
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
