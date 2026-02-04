4caa0dacea8cd08553f0bef3162fdcb719a0f8f2
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2026-02-04 18:58:19 +0530

    email and user reputation fixed

---
+++ server/Talenex.Domain/Entities/UserReviews.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talenex.Domain.Entities
+{
+    public class UserReviews : IEntity
+    {
+        public Guid Id { get; set; }
+
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+
+        public User User { get; set; }
+
+
+        [Range(0,5)]
+        public int Rating { get; set; } = 0;
+        public string ReviewMsg { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+    
+}
